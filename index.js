4cca49ddc44036306a410d90538250a5bb2e5ceb
sangavi002 <sangavibabu007@gmail.com>
2025-02-06 14:01:58 +0530

    first commit

---
+++ index.js
@@ -0,0 +1,27 @@
+const express = require("express");
+const dotenv = require("dotenv");
+dotenv.config();
+const connection = require("./config/db");
+const userRouter = require("./route/user.route")
+const cors = require("cors")
+
+const app = express();
+const port = process.env.PORT;
+
+app.use(express.json());
+app.use(cors())
+
+app.use("/user",userRouter)
+
+app.get("/",(req,res) => {
+    res.status(200).send({"msg": "Health check"})
+})
+
+app.listen(port,async() => {
+    try{
+        await connection;
+        console.log(`Server is running on port ${port} and db is connected`)
+    }catch(err){
+        console.log(err.msg)
+    }
+})
---
+++ route/user.route.js
@@ -0,0 +1,52 @@
+const express = require("express");
+const UserModel = require("../model/user.model")
+const userRouter = express.Router();
+const bcryptjs = require('bcryptjs');
+const jwt = require("jsonwebtoken")
+
+
+userRouter.post("/register", async (req, res) => {
+    const { username, password,role } = req.body;
+    try {
+        const existingUser = await UserModel.findOne({ username });
+        if (existingUser) {
+            return res.status(400).send({ "msg": "Email already exists." });
+        }
+        bcryptjs.hash(password, 10, async (err, hash) => {
+            if (err) {
+                res.status(500).send({ "msg": "Something went wrong." });
+            } else {
+                const user = new UserModel({ username, password: hash, role });
+                await user.save();
+                res.status(200).send({ "msg": "Successfully Registered." });
+            }
+        })
+    } catch (err) {
+        res.status(500).send({ "msg": "Failed to register." });
+    }
+});
+
+userRouter.post("/login",async(req,res) => {
+    const {username,password} = req.body;
+    try{
+        const user = await UserModel.findOne({username})
+        if(user){
+            bcryptjs.compare(password,user.password,async(err,result) => {
+                if(err){
+                    res.status(500).send({"msg": "Something went wrong."});
+                }if(result){
+                    let token = jwt.sign({id: user._id},process.env.JWT_SECRET)
+                    res.status(200).send({"msg":"Logged In successfully.","Token": token,"UserId":user._id});
+                }else{
+                    res.status(404).send({"msg": "Wrong paswword."});
+                }
+            })
+        }else{
+            res.status(404).send({"msg": "Wrong crendentails."})
+        }
+    }catch(err){
+        res.status(404).send({"msg": "Error in login."});
+    }
+})
+
+module.exports = userRouter
