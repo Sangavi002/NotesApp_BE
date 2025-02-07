992edb55306baeee8411d493a491606f9f3b1a3c
sangavi002 <sangavibabu007@gmail.com>
2025-02-07 14:49:15 +0530

    cors added

--- index.js
+++ index.js
@@ -11,7 +11,7 @@ const app = express();
 const port = process.env.PORT;
 
 app.use(express.json());
-app.use(cors({ origin: "http://localhost:3000" }))
+app.use(cors())
 
 app.use("/user",userRouter)
 app.use("/content",notesrouter)
